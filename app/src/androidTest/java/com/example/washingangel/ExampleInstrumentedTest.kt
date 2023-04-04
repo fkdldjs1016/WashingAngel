3fa52515e71dc9a36ced1956e5a747b5b54fa399
jordy <parkjimin1016@naver.com>
2023-04-04 13:28:37 +0900

    change bottom nav color

--- app/src/androidTest/java/com/example/washingangel/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/washingangel/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bottomnavyt", appContext.packageName)
+        assertEquals("com.example.washingangel", appContext.packageName)
     }
 }
